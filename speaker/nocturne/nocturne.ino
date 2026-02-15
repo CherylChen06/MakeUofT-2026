0fcab70e638a6cd9ead9fdb28125135229d7f915
CherylChen06 <cheryllech@gmail.com>
2026-02-15 10:08:47 -0500

    music + heart rate

--- speaker/nocturne/nocturne.ino
+++ speaker/nocturne/nocturne.ino
@@ -12,6 +12,12 @@
 #define C6 1047
 #define D6 1175
 
+#include <DFRobot_Heartrate.h>
+
+#define SENSOR_PIN A0
+
+DFRobot_Heartrate heartrate(ANALOG_MODE);
+
 int notes[] = {
   Bb4, G5, F5, G5, F5, Eb5, Bb4,
   G5, C5, D5, C5, B4, C5, C6, G5, Bb5, Ab5, G5,
@@ -27,10 +33,28 @@ int dur[] = {
 };
 
 void setup() {
+  Serial.begin(115200);
   for(int i=0;i<33;i++){
     tone(SPK, notes[i], dur[i]);
     delay(dur[i]*2.8);
+      uint16_t rate = heartrate.getValue(SENSOR_PIN);
+
+  if(rate){
+    Serial.print("Heart Rate: ");
+    Serial.println(rate/10);
+  }
+
+  delay(20);
   }
 }
 
-void loop(){}
+void loop(){
+  uint16_t rate = heartrate.getValue(SENSOR_PIN);
+
+  if(rate){
+    Serial.print("Heart Rate: ");
+    Serial.println(rate/10);
+  }
+
+  delay(500);
+}
